7635e87a708bb52d3d4bb83cc6504762dd667768
Alefiles <ales24x@gmail.com>
2021-08-17 18:36:06 -0300

    modificaciones
    modificaciones

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-//DOM
+// Cambio estilo en el nav al apretar el botón
 
-//let div = document.createElement ("div");
-//div.innerHTML="<h1> Nuestras Sabrosuras</h1>";
-//document.article.appendChild(div);
-
+/*let botonNav = document.getElementById("btnNav");
+botonNav.onmousedown = () => {
+style.backgroundColor = "lightblue";
+}*/
 //inicio el carrito
 let carrito = [];
 
@@ -19,6 +19,43 @@ const Bienvenida = $("Bienvenida");
 const tipo = $("#selecTipo");
 const catego = $("#selecCategoria");
 
+<<<<<<< Updated upstream
+=======
+// Solicito info al cliente
+let cliente = document.getElementById("cliente");
+cliente.onkeyup = () => {console.log ("apreto tecla")};
+//let clienteJson =JSON.stringify(cliente);
+//localStorage.setItem("cliente", clienteJson);
+
+
+
+//function cargarCliente(e){
+//        e.preventDefault();
+//        console.log("cliente Enviado");
+        
+//}
+//}
+//else{
+//       console.log ("no se ingresó ningún nombre");
+//}
+
+
+//const clienteGen = prompt ("¿nos quisieras indicar tu género? Femenino, Masculino, No Binario, Otro, No gracias " )
+        
+
+
+//Creo el objeto producto con su constructor
+
+function Producto (id, tipoProd, categoria, sabor, precio) {
+        this.id = parseInt(id);
+        this.tipoProd = tipoProd.toLowerCase();
+        this.categoria = categoria.toLowerCase();
+        this.sabor = sabor.toLowerCase();
+        this.precio = parseFloat(precio);
+        }
+
+// Creo los productos del store
+>>>>>>> Stashed changes
 
 //Escucho el enviar del formulario
 form.submit(function(event){
